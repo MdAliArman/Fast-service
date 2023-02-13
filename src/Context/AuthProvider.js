cbbe1b9baf1f0e60deb657340c10f9d1bb7ad435
aliarmane <aliarman1277@gmail.com>
2023-02-13 15:35:29 +0600

    fourth commit

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,23 +1,31 @@
 import React, { createContext, useState} from 'react';
-import {getAuth} from 'firebase/app'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../Fairbase/firebase';
 
 export const AuthContext=createContext();
 const auth= getAuth(app)
-const AuthProvider = ({childen}) => {
+const AuthProvider = ({children}) => {
     const [users, setUsers]=useState([])
+   const [loader, setLoader]=useState(true)
 
-
+    const createUser=(email, password)=>{
+      return createUserWithEmailAndPassword(auth, email, password)
+    };
+    const login=(email, password)=>{
+        return signInWithEmailAndPassword(auth, email, password)
+    }
 
 
 
 const info={
     users,
+    createUser,
+    login
 }
 
     return (
         <AuthContext.Provider value={info}>
-            {childen}
+          {children}
         </AuthContext.Provider>
     );
 };
